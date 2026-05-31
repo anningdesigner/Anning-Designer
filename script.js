9eb23a9ad510cfc0c8bb76496b99dfff65775645
anningdesigner <anningdesigner3515@gmail.com>
2026-05-31 20:19:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Anning Designer Website");
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let mobile = document.getElementById("mobile").value;
+
+let message =
+`Hello Anning Designer,
+
+My Name: ${name}
+
+Email: ${email}
+
+Mobile: ${mobile}
+
+I would like to know more about your Graphic Design and UI/UX Design Services.`;
+
+let whatsappUrl =
+`https://wa.me/918320453064?text=${encodeURIComponent(message)}`;
+
+window.open(whatsappUrl,"_blank");
+
+});
